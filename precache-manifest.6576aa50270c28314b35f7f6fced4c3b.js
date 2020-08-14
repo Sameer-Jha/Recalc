e1969fe7ba34e68e536c643f1b9ee20b76b3cd6d
Sameer-Jha <sameer.btech.cs17@iiitranchi.ac.in>
2020-08-14 16:31:19 +0000

    Updates

---
+++ precache-manifest.6576aa50270c28314b35f7f6fced4c3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce34911fbafc0bfa86e6b5c8047aed2a",
+    "url": "/Recalc/index.html"
+  },
+  {
+    "revision": "1abc1380e6b9158b9b4d",
+    "url": "/Recalc/static/css/main.dafda10d.chunk.css"
+  },
+  {
+    "revision": "f39efe7f1d367a991e05",
+    "url": "/Recalc/static/js/2.8ea56544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recalc/static/js/2.8ea56544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abc1380e6b9158b9b4d",
+    "url": "/Recalc/static/js/main.649900fa.chunk.js"
+  },
+  {
+    "revision": "f8903fac242f04c6c57b",
+    "url": "/Recalc/static/js/runtime-main.b36fa668.js"
+  }
+]);
